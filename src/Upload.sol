f4a792f8b6bc8b992bec7779ebb5957887dc931e
Collins Okafor <collinszurum@gmail.com>
2023-10-08 17:45:41 +0100

    chore: forge init

--- src/Upload.sol
+++ src/Upload.sol
@@ -10,7 +10,7 @@ contract Upload{
 
     mapping (address => string[]) urlValue; 
     mapping (address => mapping (address => bool)) ownership;
-    mapping (address => Access[]) accessList;
+    mapping (address => Access[]) public accessList;
     mapping (address => mapping (address => bool)) previousData;
 
     function add(address _user, string calldata url) external {  
@@ -19,12 +19,35 @@ contract Upload{
 
     function allow(address user) external {
         ownership[msg.sender][user] = true;
-        accessList[msg.sender].push(Access(user, true)); 
+        if(previousData[msg.sender][user] == true){
+            for(uint i = 0; i < accessList[msg.sender].length; i++){
+                if(accessList[msg.sender][i].user == user){
+                    accessList[msg.sender][i].access = true;
+                }
+            }
+        }else{
+            accessList[msg.sender].push(Access(user, true)); 
+            previousData[msg.sender][user] = true;
+        }
+        
     } 
 
     function disallow(address user) external {
         ownership[msg.sender][user] = false;
-        accessList[msg.sender].push(Access(user, true)); 
-    }    
+        for(uint i = 0; i < accessList[msg.sender].length; i++){
+            if(accessList[msg.sender][i].user == user){
+                accessList[msg.sender][i].access = false;
+            }
+        }
+    }   
+
+    function display(address _user) external view returns(string[] memory){
+        require(_user == msg.sender || ownership[_user][msg.sender], "You don't have access.");
+        return urlValue[_user];
+    } 
+
+    function shareAccess() public view returns(Access[] memory){
+        return accessList[msg.sender];
+    }
 
 }
